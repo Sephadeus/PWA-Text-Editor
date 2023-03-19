86b9e6eb99a95db8af9c5a21670054f53bfc5a20
Sephadeus <90430093+Sephadeus@users.noreply.github.com>
2023-03-19 19:56:35 -0400

    added and configured workbox plugins and css loaders and babel to webpack

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,8 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
 
-export const postDb = async (content) => {
+export const putDb = async (content) => {
 
 
   console.log('Update Database');
@@ -31,7 +30,6 @@ export const postDb = async (content) => {
 
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
 
 export const getDb = async() =>{
   console.log("getting database info");
