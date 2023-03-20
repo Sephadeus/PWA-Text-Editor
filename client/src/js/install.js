a8986abfe8cab31ec0f1d11a4f3d341f6d16a45f
Sephadeus <90430093+Sephadeus@users.noreply.github.com>
2023-03-19 21:04:24 -0400

    header.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -10,7 +10,7 @@ window.addEventListener('beforeinstallprompt', (event) => {
  butInstall.classList.toggle("hidden", false);
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
+// COMPLETE: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
     const promptEvent = window.deferredPrompt;
 
@@ -27,7 +27,7 @@ butInstall.addEventListener('click', async () => {
     butInstall.classList.toggle("hidden", true);
 });
 
-// TODO: Add an handler for the `appinstalled` event
+// COMPLETE: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
 
     console.log("Installed!")
