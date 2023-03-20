6be05660cb32262fc2193a288e1f8255e221a7ea
Sephadeus <90430093+Sephadeus@users.noreply.github.com>
2023-03-19 22:00:44 -0400

    one more change

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -22,7 +22,7 @@ export const putDb = async (content) => {
   const noteDb = await openDB('jate', 1);
   const tx = noteDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.add({ id: 1, value: content });
+  const request = store.put({ id: 1, value: content });
 
   const result = await request;
   console.log('Text saved!', result.value);
